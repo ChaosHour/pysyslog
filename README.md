a221f076d804c2311d14169d409ab0644cc6a01e
ChaosHour <KLarsen@reachlocal.com>
2023-12-13 11:52:19 -0800

    adding files

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # pysyslog
+
+
+
+## Description
+This was created to help parsing the syslog. Yes, there are some really good shell one-liners out there, but I wanted to be able to use this in a more programmatic way.
+
+## Usage
+
+```python
+python3 pysyslog.py -f /path/to/your/file -d 2022-01-01 -s error
+python3 pysyslog.py -f /path/to/your/file -s error
+python3 pysyslog.py -f /path/to/your/file -d $(date -d "2 days ago" +%Y-%m-%d) -s error
+python3 pysyslog.py -f /path/to/your/file -d 2022-01-01 -s error -l 10
+```
+
+# requirements for pysyslog.py
+- python3
+
+
+
+# requirements for pysyslog2.py
+- python3
+- termcolor
+
+pip install -r requirements.txt
+
---
+++ pysyslog.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re
+import argparse
+from datetime import datetime, timedelta
+
+def parse_syslog(log_line, search_string, date=None):
+    # Define the regular expression pattern to match the timestamp
+    pattern = r"(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2})"
+
+    # Use the regular expression to extract the timestamp
+    match = re.search(pattern, log_line)
+    if match:
+        timestamp_str = match.group(1)
+
+        # Parse the timestamp string into a datetime object
+        timestamp = datetime.strptime(timestamp_str, "%Y-%m-%dT%H:%M:%S")
+
+        # If a date is specified, only return the timestamp if it matches the date
+        if date and timestamp.date() != date:
+            return None
+
+        # If the search string is in the log line, return the timestamp and log line
+        if search_string in log_line:
+            return timestamp, log_line
+
+    return None
+
+def main():
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(
+        description='Parse a syslog file for a specific string and date.',
+        epilog='''Examples of usage:
+        python3 parsit.py -f /path/to/your/file -d 2022-01-01 -s error
+        python3 parsit.py -f /path/to/your/file -s error
+        python3 parsit.py -f /path/to/your/file -d $(date -d "2 days ago" +%Y-%m-%d) -s error
+        python3 parsit.py -f /path/to/your/file -d 2022-01-01 -s error -l 10
+        ''',
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument("-f", "--file", required=True, help="The file to parse")
+    parser.add_argument("-d", "--date", help="The date in question (format: YYYY-MM-DD)")
+    parser.add_argument("-s", "--search", required=True, help="The string to search for")
+    parser.add_argument("-l", "--limit", type=int, default=float('inf'), help="Limit the number of lines of output")
+    args = parser.parse_args()
+
+    # If a date is specified, parse it into a date object
+    date = datetime.strptime(args.date, "%Y-%m-%d").date() if args.date else None
+
+    # Open the file and parse each line
+    with open(args.file, "r") as f:
+        count = 0
+        for line in f:
+            if count >= args.limit:
+                break
+            result = parse_syslog(line, args.search, date)
+            if result:
+                timestamp, log_line = result
+                print("Timestamp:", timestamp)
+                print("Log line:", log_line)
+                count += 1
+
+if __name__ == "__main__":
+    main()
